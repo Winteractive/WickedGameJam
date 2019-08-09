90d86f28d8dbb2d3126d599cfbd350208f3ed986
Winteractive_Marcus <winteractiveSE@gmail.com>
2019-08-09 19:37:14 +0200

    Grid WIP

---
+++ WickedGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Rules;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        GridHolder.GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ WickedGame/Assets/Scripts/RuleSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RuleSet", menuName = "RuleSet", order = 0)]
+public class RuleSet : ScriptableObject
+{
+
+    public int GRID_WIDTH;
+    public int GRID_HEIGHT;
+
+    public Int2 OBSTACLE_AMOUNT;
+
+    public float PLAYER_SPEED;
+    public float PLAYER_HEALTH;
+    public float MONSTER_SPEED;
+    public float LIGHT_RADIUS;
+    public float HEALTH_LOSS_RATE;
+    public float HEALTH_GAIN_RATE;
+}
