21851d5036123826e9a8aae74329ee5a074d8242
Winteractive_Marcus <winteractiveSE@gmail.com>
2019-08-10 12:43:05 +0200

    Light visualisation

--- WickedGame/Assets/Scripts/Int2.cs
+++ WickedGame/Assets/Scripts/Int2.cs
@@ -15,6 +15,10 @@ public struct Int2
     {
         return Vector3Int.right * x + Vector3Int.up * y;
     }
+    public Vector3Int GetAsBoardAlignedVector3Int()
+    {
+        return Vector3Int.right * x + new Vector3Int(0, 0, 1) * y;
+    }
     public bool IsSame(Int2 otherPos)
     {
         return (x == otherPos.x && y == otherPos.y);
